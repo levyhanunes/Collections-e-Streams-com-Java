9a1f1b0f5967c220b5f1bdb56df1643c7282ed61
Levy Henrique Alves Nunes <levyhanunes@gmail.com>
2021-03-28 00:14:58 -0300

    Criando List<>

---
+++ src/main/java/com/levyhanunes/ImplementandoCollectionsEStreamsComJava/list/ExemploList.java
@@ -0,0 +1,21 @@
+package com.levyhanunes.ImplementandoCollectionsEStreamsComJava.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExemploList {
+    public static void main(String[] args){
+
+        List<String> nomes = new ArrayList<>();
+
+        nomes.add("Levy");
+        nomes.add("Rafaela");
+        nomes.add("Fabio");
+        nomes.add("Raquel");
+        nomes.add("Pedro");
+        nomes.add("Rose");
+        nomes.add("Joao");
+
+        System.out.println(nomes);
+    }
+}
---
+++ src/test/java/com/levyhanunes/ImplementandoCollectionsEStreamsComJava/ImplementandoCollectionsEStreamsComJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levyhanunes.ImplementandoCollectionsEStreamsComJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImplementandoCollectionsEStreamsComJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
